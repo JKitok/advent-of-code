dd450d3716a3899259f9723e71da2f3a11dbf80f
Jan-Erik Kitok <janerikkitok@gmail.com>
2021-11-30 20:27:37 +0100

    Add empty repo with template for first day

---
+++ runner.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from dec1.main import main as main_1
+
+# %%
+main_1()
