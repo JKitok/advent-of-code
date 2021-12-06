211e2fc5a9715f750cc0f7e66280d5261b6a432f
Jan-Erik Kitok <janerikkitok@gmail.com>
2021-12-06 08:17:38 +0100

    Add solution for day 6

--- y21/d6/main.py
+++ y21/d6/main.py
@@ -6,12 +6,26 @@ from dataclasses import dataclass
 import numpy as np
 
 
-def part1(entries):
-    pass
+def simulate(fishes, days):
+    num_count = np.array([np.sum(fishes == val) for val in range(10)]).astype(np.uint64)
+
+    for i in range(days):
+        # Spawn new fishes
+        num_count[9] = num_count[0]
+        # Reset counter for existing fishes
+        num_count[7] += num_count[0]
+        # Remove counters for day
+        num_count[0:-1] = num_count[1:]
+
+    return np.sum(num_count[:-1])
 
 
-def part2(entries):
-    pass
+def part1(fishes):
+    return simulate(fishes, 80)
+
+
+def part2(fishes):
+    return simulate(fishes, 256)
 
 
 if __name__ == "__main__":
@@ -19,5 +33,7 @@ if __name__ == "__main__":
         lines = fp.readlines()
     lines = [v.strip() for v in lines]
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    fishes = np.fromstring(lines[0], sep=",").astype(np.uint64)
+
+    print(f"Part 1: {part1(fishes)}")
+    print(f"Part 2: {part2(fishes)}")
