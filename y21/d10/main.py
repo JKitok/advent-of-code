d27cab90d06ae9546e8f8090a8835378f9bd1389
Jan-Erik Kitok <janerikkitok@gmail.com>
2021-12-10 07:53:19 +0100

    Add solution for day 8 and day 9

--- y21/d10/main.py
+++ y21/d10/main.py
@@ -5,13 +5,54 @@ from dataclasses import dataclass
 
 import numpy as np
 
+to_closed = {"(": ")", "[": "]", "{": "}", "<": ">"}
+to_open = {v: k for k, v in to_closed.items()}
 
-def part1(entries):
-    pass
+
+class IncorrectCharacter(ValueError):
+    def __init__(self, c, expected):
+        super().__init__(f"expected {expected} but found {c}")
+        self.character = c
+        self.expected = expected
 
 
-def part2(entries):
-    pass
+def check_line(line):
+    open_chars = []
+    for c in line:
+        if c in to_open.values():
+            open_chars.append(c)
+        else:
+            if open_chars[-1] == to_open[c]:
+                open_chars.pop(-1)
+            else:
+                raise IncorrectCharacter(c, to_closed[open_chars[-1]])
+
+    return [to_closed[v] for v in reversed(open_chars)]
+
+
+def part1(lines):
+    sum_ = 0
+    for line in lines:
+        try:
+            check_line(line)
+        except IncorrectCharacter as e:
+            sum_ += {")": 3, "]": 57, "}": 1197, ">": 25137}[e.character]
+
+    return sum_
+
+
+def part2(lines):
+    scores = []
+    for line in lines:
+        try:
+            remaining_chars = check_line(line)
+            score = 0
+            for c in remaining_chars:
+                score = 5 * score + {")": 1, "]": 2, "}": 3, ">": 4}[c]
+            scores.append(score)
+        except IncorrectCharacter as e:
+            pass
+    return np.median(scores)
 
 
 if __name__ == "__main__":
--- y21/d9/main.py
+++ y21/d9/main.py
@@ -6,18 +6,76 @@ from dataclasses import dataclass
 import numpy as np
 
 
-def part1(entries):
-    pass
+def part1(matrix):
+    sum_ = 0
+    for row in range(matrix.shape[0]):
+        for col in range(matrix.shape[1]):
+            down = matrix[row - 1, col] if row > 0 else 10
+            up = matrix[row + 1, col] if row < matrix.shape[0] - 1 else 10
+            left = matrix[row, col - 1] if col > 0 else 10
+            right = matrix[row, col + 1] if col < matrix.shape[1] - 1 else 10
+
+            if all([matrix[row, col] - val < 0 for val in [left, right, up, down]]):
+                sum_ += 1 + matrix[row, col]
+    return sum_
 
 
-def part2(entries):
-    pass
+@dataclass
+class Point:
+    x: int
+    y: int
 
+    def is_neighbor(self, other):
+        return abs(other.x - self.x) + abs(other.y - self.y) <= 1
+
+    def __repr__(self):
+        return f"P({self.x},{self.y})"
+
+
+def part2(lines):
+    matrix[matrix < 9] = 0
+    basins = []
+
+    for row in range(matrix.shape[0]):
+        for col in range(matrix.shape[1]):
+            p = Point(row, col)
+            if matrix[row, col] == 9:
+                continue
+            for basin in basins:
+                if any(p.is_neighbor(p2) for p2 in basin):
+                    basin.append(p)
+                    break
+            else:
+                basins.append([p])
+
+    known_basins = []
+    while len(basins):
+        current = basins.pop()
+        found = False
+        for p in current:
+            if not found:
+                for basin in basins:
+                    if any(p.is_neighbor(p2) for p2 in basin):
+                        basin.extend(current)
+                        found = True
+                        break
+        if not found:
+            known_basins.append(current)
+
+    for i, basin in enumerate(known_basins):
+        for p in basin:
+            matrix[p.x, p.y] = i
+
+    sizes = sorted([len(b) for b in known_basins], reverse=True)
+    return np.prod(sizes[:3])
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.strip() for v in lines]
-
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    comma_separated = [",".join(line) for line in lines]
+    matrix = np.fromstring(",".join(comma_separated), sep=",").reshape(
+        len(lines), len(lines[0])
+    )
+    print(f"Part 1: {part1(matrix)}")
+    print(f"Part 2: {part2(matrix)}")
