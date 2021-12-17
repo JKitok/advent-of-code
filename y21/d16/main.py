259a5b5decdafb43566ed142aae1b2d73c852fa5
Jan-Erik Kitok <janerikkitok@gmail.com>
2021-12-17 08:32:53 +0100

    Add solution for day 16

--- y21/d16/main.py
+++ y21/d16/main.py
@@ -6,12 +6,59 @@ from dataclasses import dataclass
 import numpy as np
 
 
-def part1(entries):
-    pass
+def parse_literal(bits):
+    res = ""
+    idx = 0
+    stop = False
+    while not stop:
+        next_ = bits[idx : idx + 5]
+        res += next_[1:]
+        idx += 5
+        stop = next_[0] != "1"
+    res = int(res, 2)
+    return res, bits[idx:]
 
 
-def part2(entries):
-    pass
+def parse_packet(bits):
+    version = int(bits[:3], 2)
+    version_total = version
+    type_ = int(bits[3:6], 2)
+    result = 0
+    if type_ == 4:
+        result, bits = parse_literal(bits[6:])
+    else:
+        all_results = []
+        if bits[6] == "0":
+            length = int(bits[7 : 7 + 15], 2)
+            subpackets = bits[7 + 15 : 7 + 15 + length]
+            bits = bits[7 + 15 + length :]
+            while len(subpackets) > 7:
+                ver, result, subpackets = parse_packet(subpackets)
+                all_results.append(result)
+                version_total += ver
+        else:
+            numpackets = int(bits[7 : 7 + 11], 2)
+            bits = bits[7 + 11 :]
+            for i in range(numpackets):
+                ver, result, bits = parse_packet(bits)
+                all_results.append(result)
+                version_total += ver
+
+        if type_ == 0:
+            result = sum(all_results)
+        elif type_ == 1:
+            result = np.prod(all_results)
+        elif type_ == 2:
+            result = np.min(all_results)
+        elif type_ == 3:
+            result = np.max(all_results)
+        elif type_ == 5:
+            result = 1 if all_results[0] > all_results[1] else 0
+        elif type_ == 6:
+            result = 1 if all_results[0] < all_results[1] else 0
+        elif type_ == 7:
+            result = 1 if all_results[0] == all_results[1] else 0
+    return version_total, result, bits
 
 
 if __name__ == "__main__":
@@ -19,5 +66,15 @@ if __name__ == "__main__":
         lines = fp.readlines()
     lines = [v.strip() for v in lines]
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    hexstring = lines[0]
+    bitstring = "".join((bin(int(v, 16))[2:].zfill(4) for v in hexstring))
+
+    version_total = 0
+    result_total = 0
+    while len(bitstring) > 7:
+        ver, result, bitstring = parse_packet(bitstring)
+        version_total += ver
+        result_total += result
+
+    print(f"Part 1: {version_total}")
+    print(f"Part 2: {result_total}")
