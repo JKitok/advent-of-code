850e0c90e6d0d213b05da4c4be02901a5b149035
Jan-Erik Kitok <janerikkitok@gmail.com>
2021-12-25 13:46:09 +0100

    Add solution for day 25

--- y21/d25/main.py
+++ y21/d25/main.py
@@ -1,23 +1,29 @@
 import os
-import re
-
-from dataclasses import dataclass
 
 import numpy as np
 
 
-def part1(entries):
-    pass
-
-
-def part2(entries):
-    pass
+def part1(floor):
+    num = 0
+    while True:
+        old_floor = np.copy(floor)
+        for val, axis in [[1, 1], [2, 0]]:
+            cucumbers = (floor == val).reshape(floor.shape)
+            is_empty = (floor == 0) & np.roll(cucumbers, 1, axis=axis)
+            floor[np.roll(is_empty, -1, axis=axis)] = 0
+            floor[is_empty] = val
+        num += 1
+        if np.array_equal(old_floor, floor):
+            break
+    return num
 
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
-    lines = [v.strip() for v in lines]
+    lines = [v.replace(".", "0").replace(">", "1").replace("v", "2") for v in lines]
+    lines = [" ".join(v.strip()) for v in lines]
+    data = np.fromstring(" ".join(lines), sep=" ").astype(int)
+    data = data.reshape((len(lines), int(len(data) / len(lines))))
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    print(f"Part 1: {part1(data)}")
