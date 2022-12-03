3b8741d745ff3c548d529f5f36c08f272cb4fcd3
Jan-Erik Kitok <janerikkitok@gmail.com>
2022-12-03 09:15:30 +0100

    Add templates for 2022

---
+++ y22/d23/main.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+from dataclasses import dataclass
+
+import numpy as np
+
+
+def part1(entries):
+    pass
+
+
+def part2(entries):
+    pass
+
+
+if __name__ == "__main__":
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
+        lines = fp.readlines()
+
+    lines = [v.strip() for v in lines]
+
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
