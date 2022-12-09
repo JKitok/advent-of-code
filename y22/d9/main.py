41fae951ff517215fca9d32192a340182629ae62
Jan-Erik Kitok <janerikkitok@gmail.com>
2022-12-09 08:45:47 +0100

    Add solution for day 9

--- y22/d9/main.py
+++ y22/d9/main.py
@@ -1,24 +1,47 @@
 import os
-import re
-
-from dataclasses import dataclass
+import math
 
 import numpy as np
 
-
-def part1(entries):
-    pass
+sign = lambda x: int(math.copysign(1, x))
 
 
-def part2(entries):
-    pass
+def follow(Hx, Hy, Tx, Ty):
+    dx = Hx - Tx
+    dy = Hy - Ty
+    if abs(dx) > 1:
+        if abs(dy) > 0:
+            return Tx + sign(dx), Ty + sign(dy)
+        else:
+            return Tx + sign(dx), Ty
+    elif abs(dy) > 1:
+        if abs(dx) > 0:
+            return Tx + sign(dx), Ty + sign(dy)
+        else:
+            return Tx, Ty + sign(dy)
+    return Tx, Ty
+
+
+def move(instructions, N):
+    visited = set()
+    positions = [[0, 0] for i in range(N)]
+    for instr in instructions:
+        direction, n = instr.split(" ")
+        dHx, dHy = {"R": (1, 0), "D": (0, -1), "L": (-1, 0), "U": (0, 1)}[direction]
+        for i in range(int(n)):
+            positions[0][0] += dHx
+            positions[0][1] += dHy
+            for t in range(1, N):
+                positions[t] = follow(*positions[t - 1], *positions[t])
+            visited.add(positions[-1])
+
+    return len(visited)
 
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
 
-    lines = [v.strip() for v in lines]
-
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    instructions = [v.strip() for v in lines]
+    print(f"Part 1: {move(instructions, N=2)}")
+    print(f"Part 2: {move(instructions, N=10)}")
