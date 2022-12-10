509ea91b8f4fc8f6325d767ef394b66ab8a5de41
Jan-Erik Kitok <janerikkitok@gmail.com>
2022-12-10 08:07:21 +0100

    Add solution for day 10

--- y22/d10/main.py
+++ y22/d10/main.py
@@ -1,17 +1,40 @@
 import os
-import re
-
-from dataclasses import dataclass
-
-import numpy as np
 
 
-def part1(entries):
-    pass
+def parse_changes(lines):
+    cycle = 1
+    changes = {1: 1}
+    for line in lines:
+        if line == "noop":
+            cycle += 1
+        elif line.startswith("addx"):
+            _, num = line.split(" ")
+            cycle += 2
+            changes[cycle] = int(num)
+    return changes
 
 
-def part2(entries):
-    pass
+def part1(changes):
+    ret = 0
+    for i in [20, 60, 100, 140, 180, 220]:
+        X = sum((v for (k, v) in changes.items() if k <= i))
+        ret += i * X
+    return ret
+
+
+def part2(changes):
+    cnt = 0
+    drawing = ""
+    for row in range(0, 6):
+        for col in range(0, 40):
+            cnt += 1
+            X = sum((v for (k, v) in changes.items() if k <= cnt))
+            if X - 1 <= col <= X + 1:
+                drawing += "#"
+            else:
+                drawing += "."
+        drawing += "\n"
+    print(drawing)
 
 
 if __name__ == "__main__":
@@ -19,6 +42,7 @@ if __name__ == "__main__":
         lines = fp.readlines()
 
     lines = [v.strip() for v in lines]
+    changes = parse_changes(lines)
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    print(f"Part 1: {part1(changes)}")
+    print(f"Part 2: {part2(changes)}")
