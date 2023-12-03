6031a513076361fa8877d650119a4e65762bba96
Jan-Erik Kitok <janerikkitok@gmail.com>
2023-12-03 09:24:48 +0100

    Add day 3 - part 1

--- y23/d3/main.py
+++ y23/d3/main.py
@@ -6,8 +6,41 @@ from dataclasses import dataclass
 import numpy as np
 
 
+def check_neighbors(lines, i, j, n):
+    for ii in [i - 1, i, i + 1]:
+        for jj in range(j - 1, j + n + 1):
+            s = lines[ii][jj]
+            if s != "." and not s.isdigit():
+                return True
+    return False
+
+
 def part1(lines):
-    pass
+    sum_ = 0
+    i = 1
+    j = 1
+    while True:
+        if i >= len(lines) - 1:
+            return sum_
+        if j >= len(lines[0]) - 1:
+            i += 1
+            j = 1
+            continue
+
+        if not lines[i][j].isdigit():
+            j += 1
+            continue
+        else:
+            n = 0
+            while lines[i][j + n].isdigit():
+                n += 1
+            num = int(lines[i][j : j + n])
+            # Check neighbors
+            if check_neighbors(lines, i, j, n):
+                sum_ += num
+            j += n + 1
+
+    return sum_
 
 
 def part2(lines):
@@ -18,7 +51,11 @@ if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
 
-    lines = [v.strip() for v in lines]
+    lines = ["." + v.strip() + "." for v in lines]
+    # Do some padding
+    empty_line = "." * len(lines[0])
+    lines = [empty_line, *lines, empty_line]
+    print(lines)
 
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
