d1581f08a4fbb78ee33fcafccc1c1e7c1646d59d
Jan-Erik Kitok <janerikkitok@gmail.com>
2023-12-04 08:52:37 +0100

    Add solution for day  4

--- y23/d4/main.py
+++ y23/d4/main.py
@@ -1,17 +1,38 @@
 import os
-import re
 
-from dataclasses import dataclass
+from collections import defaultdict
 
 import numpy as np
 
 
-def part1(lines):
-    pass
+def part1(cards):
+    sum_ = 0
+    for winning, have in cards.values():
+        N = np.intersect1d(winning, have).size
+        if N > 0:
+            sum_ += 2 ** (N - 1)
+    return sum_
 
 
-def part2(lines):
-    pass
+def part2(cards):
+    num_cards = defaultdict(lambda: 1)
+    for i, (winning, have) in cards.items():
+        N = np.intersect1d(winning, have).size
+        for j in range(i + 1, i + 1 + N):
+            num_cards[j] += num_cards[i]
+
+    return sum(num_cards.values())
+
+
+def parse(lines):
+    cards = {}
+    for i, line in enumerate(lines):
+        _, list_ = map(str.strip, line.split(":"))
+        winning, have = map(str.strip, list_.split("|"))
+        winning = np.fromstring(winning, sep=" ")
+        have = np.fromstring(have, sep=" ")
+        cards[i + 1] = [winning, have]
+    return cards
 
 
 if __name__ == "__main__":
@@ -19,6 +40,7 @@ if __name__ == "__main__":
         lines = fp.readlines()
 
     lines = [v.strip() for v in lines]
+    cards = parse(lines)
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    print(f"Part 1: {part1(cards)}")
+    print(f"Part 2: {part2(cards)}")
