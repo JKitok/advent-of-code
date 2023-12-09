cf7ffcf25b1a19be7ff7e314ca046b7d424c503d
Jan-Erik Kitok <janerikkitok@gmail.com>
2023-12-09 08:15:52 +0100

    Add solution for day 8

--- y23/d8/main.py
+++ y23/d8/main.py
@@ -1,17 +1,44 @@
 import os
 import re
+import math
 
-from dataclasses import dataclass
-
-import numpy as np
-
-
-def part1(lines):
-    pass
+from itertools import cycle
 
 
-def part2(lines):
-    pass
+def part1(instructions, map_):
+    instr = cycle(instructions)
+    steps = 0
+    loc = "AAA"
+    while loc != "ZZZ":
+        i = next(instr)
+        loc = map_[loc][i]
+        steps += 1
+    return steps
+
+
+def part2(instructions, map_):
+    instr = cycle(instructions)
+    steps = 0
+    nodes = [n for n in map_.keys() if n.endswith("A")]
+    ends = [0] * len(nodes)
+    while any((e == 0 for e in ends)):
+        i = next(instr)
+        nodes = [map_[n][i] for n in nodes]
+        steps += 1
+        for i, node in enumerate(nodes):
+            if ends[i] == 0 and node.endswith("Z"):
+                ends[i] = steps
+    return math.lcm(*ends)
+
+
+def parse(map_lines):
+    map_ = {}
+    for line in map_lines:
+        a, b, c = re.match(
+            r"([A-Z1-9]+) \= \(([A-Z1-9]+), ([A-Z1-9]+)\)", line
+        ).groups()
+        map_[a] = {"L": b, "R": c}
+    return map_
 
 
 if __name__ == "__main__":
@@ -19,6 +46,8 @@ if __name__ == "__main__":
         lines = fp.readlines()
 
     lines = [v.strip() for v in lines]
+    instructions, _, *map_lines = lines
+    map_ = parse(map_lines)
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    print(f"Part 1: {part1(instructions, map_)}")
+    print(f"Part 2: {part2(instructions, map_)}")
