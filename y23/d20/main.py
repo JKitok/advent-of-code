5e07c9f66cd933d39edfb37707ab4e981954de3e
Jan-Erik Kitok <janerikkitok@gmail.com>
2023-12-20 21:25:55 +0100

    Add solution for day 20 - part 1

--- y23/d20/main.py
+++ y23/d20/main.py
@@ -1,17 +1,119 @@
 import os
-import re
-
+import copy
 from dataclasses import dataclass
-
-import numpy as np
-
-
-def part1(lines):
-    pass
+from enum import Enum
+from typing import Optional, List, Tuple
+from queue import Queue
 
 
-def part2(lines):
-    pass
+class SignalState(Enum):
+    LOW = 0
+    HIGH = 1
+
+
+@dataclass
+class Signal:
+    state: SignalState
+    source: str
+
+
+class Module:
+    def __init__(self, name: str, outputs: List[str]):
+        self.name = name
+        self.outputs = outputs
+
+    def connect_input(self, module: str):
+        pass
+
+    def receive(self, signal: Signal) -> Optional[Signal]:
+        pass
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.outputs})"
+
+
+class FlipFlop(Module):
+    def __init__(self, name: str, outputs: List[str]):
+        super().__init__(name, outputs)
+        self.on = False
+
+    def receive(self, signal: Signal):
+        if signal.state == SignalState.HIGH:
+            return
+        elif not self.on:
+            self.on = True
+            return Signal(SignalState.HIGH, self.name)
+        else:
+            self.on = False
+            return Signal(SignalState.LOW, self.name)
+
+
+class Conjunction(Module):
+    def __init__(self, name: str, outputs: List[str]):
+        super().__init__(name, outputs)
+        self.inputs = {}
+
+    def connect_input(self, module: str):
+        self.inputs[module] = SignalState.LOW
+
+    def receive(self, signal: Signal):
+        self.inputs[signal.source] = signal.state
+        if all((v == SignalState.HIGH for v in self.inputs.values())):
+            return Signal(SignalState.LOW, self.name)
+        else:
+            return Signal(SignalState.HIGH, self.name)
+
+
+class BroadCaster(Module):
+    def receive(self, signal: Signal):
+        return Signal(signal.state, self.name)
+
+
+def parse(lines):
+    modules = {}
+    for line in lines:
+        module_str, connection_str = map(str.strip, line.split(">"))
+        module_str = module_str[:-1].strip()
+        connections = list(map(str.strip, connection_str.split(",")))
+        if module_str == "broadcaster":
+            modules["broadcaster"] = BroadCaster("broadcaster", connections)
+        elif module_str.startswith("%"):
+            name = module_str[1:]
+            modules[name] = FlipFlop(name, connections)
+        elif module_str.startswith("&"):
+            name = module_str[1:]
+            modules[name] = Conjunction(name, connections)
+        else:
+            raise ValueError(module_str)
+
+    for name, module in modules.items():
+        for c in module.outputs:
+            if c in modules:
+                modules[c].connect_input(name)
+    return modules
+
+
+def run(modules, NUM):
+    signals = []
+    q = Queue()
+    N = 0
+    while N < NUM:
+        # Press button
+        q.put(("broadcaster", Signal(SignalState.LOW, "button")))
+        N += 1
+        # Handle all signals
+        while not q.empty():
+            receiver, signal = q.get()
+            signals.append((receiver, signal))
+            rec = modules.get(receiver)
+            if rec:
+                signal = rec.receive(signal)
+                if signal:
+                    for o in rec.outputs:
+                        q.put((o, signal))
+    num_low = sum((s.state == SignalState.LOW for _, s in signals))
+    num_high = sum((s.state == SignalState.HIGH for _, s in signals))
+    return num_low * num_high
 
 
 if __name__ == "__main__":
@@ -19,6 +121,6 @@ if __name__ == "__main__":
         lines = fp.readlines()
 
     lines = [v.strip() for v in lines]
+    modules = parse(lines)
 
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    print(f"Part 1: {run(copy.deepcopy(modules), NUM=1000)}")
