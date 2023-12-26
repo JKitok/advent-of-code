d0bf1af479fe5257401660428fe4e2d892cf9f32
Jan-Erik Kitok <janerikkitok@gmail.com>
2023-12-26 10:14:15 +0100

    Add solution for day 21 part 2

--- y23/d21/main.py
+++ y23/d21/main.py
@@ -1,79 +1,43 @@
 import os
-from tqdm import tqdm
 import numpy as np
+import pickle
 
 
-def part1(array, N=64):
+THISDIR = os.path.dirname(__file__)
+
+
+def run(array, num=64, dump=False):
+    N, M = array.shape
     res = np.where(array == "S")
     x0 = res[0][0]
     x1 = res[1][0]
     positions = set()
     positions.add((x0, x1))
     i = 0
-    while i < N:
+    nums = {}
+    while i < num:
         new_set = set()
         for x0, x1 in positions:
             for dx0, dx1 in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 x0n = x0 + dx0
                 x1n = x1 + dx1
-                if not 0 <= x0n < array.shape[0] or not 0 <= x1n < array.shape[1]:
-                    continue
-                if array[x0n, x1n] in [".", "S"]:
+                if array[x0n % N, x1n % M] in [".", "S"]:
                     new_set.add((x0n, x1n))
         i += 1
         positions = new_set
+        nums[i] = len(positions)
+    if dump:
+        with open(os.path.join(THISDIR, "out.pickle"), "wb") as fp:
+            pickle.dump(nums, fp)
     return len(positions)
 
 
-def fix(num):
-    v = np.int64(str(num).replace("2", "1").replace("3", "1").replace("4", "1"))
-    return v
-
-
-def part2(array, N=10):
-    res = np.where(array == "S")
-    x0 = res[0][0]
-    x1 = res[1][0]
-    nums = np.zeros_like(array, dtype=np.int64)
-    nums[x0, x1] = 1
-    mask = np.zeros_like(array, dtype=np.bool_)
-    mask[np.where(array == ".")] = 1
-    mask[x0, x1] = 1
-    for i in tqdm(range(N)):
-        new_nums = np.zeros_like(nums)
-        new_nums[1:, :] += nums[:-1, :]
-        new_nums[:-1, :] += nums[1:, :]
-        new_nums[:, 1:] += nums[:, :-1]
-        new_nums[:, :-1] += nums[:, 1:]
-        new_nums[0, :] += 10 * nums[-1, :]
-        new_nums[-1, :] += 10 * nums[0, :]
-        new_nums[:, 0] = 10 * nums[:, -1]
-        new_nums[:, -1] = 10 * nums[:, 0]
-        new_nums = np.multiply(mask, new_nums)
-        indices = np.where(new_nums > 0)
-        for i, j in zip(indices[0], indices[1]):
-            new_nums[i, j] = fix(new_nums[i, j])
-        print(nums)
-        nums = new_nums
-
-    N = 0
-    non_zero = np.where(nums > 0)
-    for i, j in zip(indices[0], indices[1]):
-        val = nums[i, j]
-        while val > 0:
-            is_val = val % 10
-            if is_val:
-                N += 1
-            val //= 10
-
-    return N
-
-
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(THISDIR, "input.txt")) as fp:
         lines = fp.readlines()
 
     lines = [list(v.strip()) for v in lines]
     arr = np.array(lines, dtype=np.str_)
-    # print(f"Part 1: {part1(arr)}")
-    print(f"Part 2: {part2(arr)}")
+    print(f"Part 1: {run(arr, num=64)}")
+    run(arr, num=1000, dump=True)
+    print("Part 2 was solved in a notebook, so go look there...")
