ed36a22e4c29adcf3c59eaca0ba3ac821d55aadf
Jan-Erik Kitok <janerikkitok@gmail.com>
2024-12-03 20:05:34 +0100

    Add day 3

--- y24/d3/main.py
+++ y24/d3/main.py
@@ -1,19 +1,38 @@
 import os
 import copy
-from dataclasses import dataclass
-import numpy as np
+import re
 
 
 def part1(lines):
-    pass
+    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+    res = 0
+    for line in lines:
+        all_ = re.findall(pattern, line)
+        for group in all_:
+            res += int(group[0]) * int(group[1])
+    return res
 
 
 def part2(lines):
-    pass
+    pattern = re.compile(r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\))")
+    res = 0
+    enabled = True
+    for line in lines:
+        all_ = re.findall(pattern, line)
+        for group in all_:
+            if group[0] == "do()":
+                enabled = True
+            elif group[0] == "don't()":
+                enabled = False
+            else:
+                assert group[0].startswith("mul")
+                if enabled:
+                    res += int(group[1]) * int(group[2])
+    return res
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
     print(f"Part 1: {part1(copy.deepcopy(lines))}")
