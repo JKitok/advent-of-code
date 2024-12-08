c582ed0c38d98500c45957ffe86bdf68d044fb77
Jan-Erik Kitok <janerikkitok@gmail.com>
2024-12-08 11:15:25 +0100

    Add solutions for days 8-9

--- y24/d8/main.py
+++ y24/d8/main.py
@@ -1,20 +1,44 @@
 import os
-import copy
-from dataclasses import dataclass
-import numpy as np
+import itertools
+from tqdm.contrib import tzip
 
 
-def part1(lines):
-    pass
+def parse(lines):
+    test_values, numbers = [], []
+    for line in lines:
+        test, rest = map(str.strip, line.split(":"))
+        test_values.append(int(test))
+        numbers.append([*map(int, rest.split())])
+    return test_values, numbers
 
 
-def part2(lines):
-    pass
+def run(test_values, numbers, operators):
+    sum_ = 0
+    for test, nums in tzip(test_values, numbers):
+        for operations in itertools.product(operators, repeat=len(nums) - 1):
+            start = nums[0]
+            for op, n in zip(operations, nums[1:]):
+                if start > test:
+                    break
+                if op == "*":
+                    start *= n
+                elif op == "+":
+                    start += n
+                elif op == "||":
+                    start = int(str(start) + str(n))
+                else:
+                    raise ValueError(op)
+
+            if start == test:
+                sum_ += test
+                break
+    return sum_
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
-    print(f"Part 1: {part1(copy.deepcopy(lines))}")
-    print(f"Part 2: {part2(copy.deepcopy(lines))}")
+    test_values, numbers = parse(lines)
+    print(f"Part 1: {run(test_values, numbers, operators=['+', '*'])}")
+    print(f"Part 2: {run(test_values, numbers, operators=['+', '*', '||'])}")
--- y24/d9/main.py
+++ y24/d9/main.py
@@ -1,20 +1,47 @@
 import os
 import copy
-from dataclasses import dataclass
-import numpy as np
+from collections import defaultdict
+import itertools
 
 
-def part1(lines):
-    pass
+def run(lines, resonance=False):
+    N = len(lines[0])
+    M = len(lines)
+    antennas = defaultdict(lambda: [])
+    anti_nodes = defaultdict(lambda: [])
+    for i, line in enumerate(lines):
+        for j, v in enumerate(line):
+            if v != ".":
+                antennas[v].append((i, j))
 
+    for frequency, locations in antennas.items():
+        for p1, p2 in itertools.combinations(locations, 2):
+            dn = p2[0] - p1[0]
+            dm = p2[1] - p1[1]
 
-def part2(lines):
-    pass
+            for point, direction in ((p2, 1), (p1, -1)):
+                if resonance:
+                    anti_nodes[point].append(frequency)
+                factor = 1
+                while True:
+                    node = (
+                        point[0] + direction * factor * dn,
+                        point[1] + direction * factor * dm,
+                    )
+                    if 0 <= node[0] < N and 0 <= node[1] < M:
+                        anti_nodes[node].append(frequency)
+                        factor += 1
+                        if not resonance:
+                            break
+                    else:
+                        break
+
+    return len(anti_nodes.items())
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
-    print(f"Part 1: {part1(copy.deepcopy(lines))}")
-    print(f"Part 2: {part2(copy.deepcopy(lines))}")
+    print(f"Part 1: {run(copy.deepcopy(lines), resonance=False)}")
+    print(f"Part 2: {run(copy.deepcopy(lines), resonance=True)}")
