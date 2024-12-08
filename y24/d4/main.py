59171e583079aa67fd0ce2b2a5b39cecefa6ae16
Jan-Erik Kitok <janerikkitok@gmail.com>
2024-12-08 21:03:06 +0100

    Add days 4-5

--- y24/d4/main.py
+++ y24/d4/main.py
@@ -1,20 +1,73 @@
 import os
-import copy
-from dataclasses import dataclass
 import numpy as np
 
 
-def part1(lines):
-    pass
+def part1(grid):
+    SEARCH_ARR = np.array(["X", "M", "A", "S"])
+    REVERSED_ARR = SEARCH_ARR[::-1]
+    N = len(SEARCH_ARR)
+    # Vertical search
+    num = 0
+    for i in range(0, grid.shape[0] - (N - 1)):
+        for j in range(0, grid.shape[1]):
+            if (SEARCH_ARR == grid[i : i + N, j]).all():
+                num += 1
+            elif (REVERSED_ARR == grid[i : i + N, j]).all():
+                num += 1
+    # Horizontal search
+    for j in range(0, grid.shape[1] - (N - 1)):
+        for i in range(0, grid.shape[0]):
+            if (SEARCH_ARR == grid[i, j : j + N]).all():
+                num += 1
+            elif (REVERSED_ARR == grid[i, j : j + N]).all():
+                num += 1
+    # Diagonal search one way
+    for i in range(0, grid.shape[0] - (N - 1)):
+        for j in range(0, grid.shape[1] - (N - 1)):
+            iv = [i, i + 1, i + 2, i + 3]
+            jv = [j, j + 1, j + 2, j + 3]
+            if (SEARCH_ARR == grid[iv, jv]).all():
+                num += 1
+            elif (REVERSED_ARR == grid[iv, jv]).all():
+                num += 1
+    # Diagonal search other way
+    for i in range(N - 1, grid.shape[0]):
+        for j in range(0, grid.shape[1] - (N - 1)):
+            iv = [i, i - 1, i - 2, i - 3]
+            jv = [j, j + 1, j + 2, j + 3]
+            if (SEARCH_ARR == grid[iv, jv]).all():
+                num += 1
+            elif (REVERSED_ARR == grid[iv, jv]).all():
+                num += 1
+    return num
 
 
-def part2(lines):
-    pass
+def part2(grid):
+    search_arr = np.array(["M", "A", "S"])
+    reversed_arr = search_arr[::-1]
+    num = 0
+    for i in range(1, grid.shape[0] - 1):
+        for j in range(1, grid.shape[1] - 1):
+            # First diagonal
+            iv = [i - 1, i, i + 1]
+            jv = [j - 1, j, j + 1]
+            if (search_arr == grid[iv, jv]).all() or (
+                reversed_arr == grid[iv, jv]
+            ).all():
+                # Second diagonal
+                iv = [i + 1, i, i - 1]
+                jv = [j - 1, j, j + 1]
+                if (search_arr == grid[iv, jv]).all() or (
+                    reversed_arr == grid[iv, jv]
+                ).all():
+                    num += 1
+    return num
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
-    print(f"Part 1: {part1(copy.deepcopy(lines))}")
-    print(f"Part 2: {part2(copy.deepcopy(lines))}")
+    grid = np.array([list(line) for line in lines])
+    print(f"Part 1: {part1(grid)}")
+    print(f"Part 2: {part2(grid)}")
--- y24/d5/main.py
+++ y24/d5/main.py
@@ -1,20 +1,68 @@
 import os
-import copy
-from dataclasses import dataclass
-import numpy as np
+import math
+
+from collections import defaultdict
+from functools import cmp_to_key
 
 
-def part1(lines):
-    pass
+def part1(rules, update_list):
+    sum_ = 0
+    for numbers in update_list:
+        if check_ok(rules, numbers):
+            idx = math.floor(len(numbers) / 2)
+            sum_ += numbers[idx]
+    return sum_
 
 
-def part2(lines):
-    pass
+def part2(rules, update_list):
+    sum_ = 0
+    for numbers in update_list:
+        if not check_ok(rules, numbers):
+            numbers = sorted(
+                numbers,
+                key=cmp_to_key(
+                    lambda v1, v2: -1 if check_order_correct(rules, v1, v2) else 1
+                ),
+            )
+            idx = math.floor(len(numbers) / 2)
+            sum_ += numbers[idx]
+    return sum_
+
+
+def check_ok(rules, numbers):
+    for i, n in enumerate(numbers[:-1]):
+        for after in numbers[i + 1 :]:
+            if not check_order_correct(rules, n, after):
+                return False
+    return True
+
+
+def check_order_correct(rules, n1, n2):
+    one_before_two = n2 in rules[n1]
+    two_before_one = n1 in rules[n2]
+    return one_before_two and not two_before_one
+
+
+def parse(lines):
+    lines = [v.rstrip("\n") for v in lines]
+    idx = lines.index("")
+    rules_list = lines[:idx]
+    updates_list = lines[idx + 1 :]
+
+    rules = defaultdict(list)
+    for txt in rules_list:
+        before, after = map(int, txt.split("|"))
+        rules[before].append(after)
+
+    updates = []
+    for txt in updates_list:
+        updates.append([*map(int, txt.split(","))])
+    return rules, updates
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
-    lines = [v.rstrip("\n") for v in lines]
-    print(f"Part 1: {part1(copy.deepcopy(lines))}")
-    print(f"Part 2: {part2(copy.deepcopy(lines))}")
+    rules, updates = parse(lines)
+    print(f"Part 1: {part1(rules, updates)}")
+    print(f"Part 2: {part2(rules, updates)}")
