1b88e54316c44a291f384b124de1e1a409cbae2f
Jan-Erik Kitok <janerikkitok@gmail.com>
2024-12-09 13:19:43 +0100

    Fix off-by-one-error in days :)

--- y24/d7/main.py
+++ y24/d7/main.py
@@ -1,76 +1,44 @@
 import os
 import itertools
-
-import numpy as np
-import tqdm
-
-ORIENTATIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+from tqdm.contrib import tzip
 
 
-def get_guard_position(grid):
-    guard_idx = np.where(grid == "^")
-    x1 = guard_idx[0][0]
-    x2 = guard_idx[1][0]
-    return x1, x2
+def parse(lines):
+    test_values, numbers = [], []
+    for line in lines:
+        test, rest = map(str.strip, line.split(":"))
+        test_values.append(int(test))
+        numbers.append([*map(int, rest.split())])
+    return test_values, numbers
 
 
-def part1(grid):
-    visited = np.zeros_like(grid, dtype=int)
-    x1, x2 = get_guard_position(grid)
-    direction_iter = itertools.cycle(ORIENTATIONS)
-    direction = next(direction_iter)
-    while True:
-        visited[x1, x2] = 1
-        new_x1 = x1 + direction[0]
-        new_x2 = x2 + direction[1]
-        if not ((0 <= new_x1 < grid.shape[0]) and (0 <= new_x2 < grid.shape[1])):
-            break
-        elif grid[new_x1, new_x2] != "#":
-            x1, x2 = new_x1, new_x2
-        else:
-            direction = next(direction_iter)
-    return np.sum(np.sum(visited))
-
-
-def part2(grid):
-    N = 0
-    for obstacle_x1 in tqdm.tqdm(range(grid.shape[0])):
-        for obstacle_x2 in range(grid.shape[1]):
-            if grid[obstacle_x1, obstacle_x2] != ".":
-                continue
-            x1, x2 = get_guard_position(grid)
-            direction_iter = itertools.cycle(ORIENTATIONS)
-            direction = next(direction_iter)
-            position_and_direction = set()
-            while True:
-                idx = (x1, x2, *direction)
-                if idx in position_and_direction:
-                    N += 1
+def run(test_values, numbers, operators):
+    sum_ = 0
+    for test, nums in tzip(test_values, numbers):
+        for operations in itertools.product(operators, repeat=len(nums) - 1):
+            start = nums[0]
+            for op, n in zip(operations, nums[1:]):
+                if start > test:
                     break
+                if op == "*":
+                    start *= n
+                elif op == "+":
+                    start += n
+                elif op == "||":
+                    start = int(str(start) + str(n))
                 else:
-                    position_and_direction.add(idx)
-                new_x1 = x1 + direction[0]
-                new_x2 = x2 + direction[1]
+                    raise ValueError(op)
 
-                if not (
-                    (0 <= new_x1 < grid.shape[0]) and (0 <= new_x2 < grid.shape[1])
-                ):
-                    break
-                elif grid[new_x1, new_x2] != "#" and (new_x1, new_x2) != (
-                    obstacle_x1,
-                    obstacle_x2,
-                ):
-                    x1, x2 = new_x1, new_x2
-                else:
-                    direction = next(direction_iter)
-    return N
+            if start == test:
+                sum_ += test
+                break
+    return sum_
 
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
-    grid = np.array([list(line) for line in lines])
-
-    print(f"Part 1: {part1(grid)}")
-    print(f"Part 2: {part2(grid)}")
+    test_values, numbers = parse(lines)
+    print(f"Part 1: {run(test_values, numbers, operators=['+', '*'])}")
+    print(f"Part 2: {run(test_values, numbers, operators=['+', '*', '||'])}")
--- y24/d8/main.py
+++ y24/d8/main.py
@@ -1,44 +1,47 @@
 import os
+import copy
+from collections import defaultdict
 import itertools
-from tqdm.contrib import tzip
 
 
-def parse(lines):
-    test_values, numbers = [], []
-    for line in lines:
-        test, rest = map(str.strip, line.split(":"))
-        test_values.append(int(test))
-        numbers.append([*map(int, rest.split())])
-    return test_values, numbers
+def run(lines, resonance=False):
+    N = len(lines[0])
+    M = len(lines)
+    antennas = defaultdict(lambda: [])
+    anti_nodes = defaultdict(lambda: [])
+    for i, line in enumerate(lines):
+        for j, v in enumerate(line):
+            if v != ".":
+                antennas[v].append((i, j))
 
+    for frequency, locations in antennas.items():
+        for p1, p2 in itertools.combinations(locations, 2):
+            dn = p2[0] - p1[0]
+            dm = p2[1] - p1[1]
 
-def run(test_values, numbers, operators):
-    sum_ = 0
-    for test, nums in tzip(test_values, numbers):
-        for operations in itertools.product(operators, repeat=len(nums) - 1):
-            start = nums[0]
-            for op, n in zip(operations, nums[1:]):
-                if start > test:
-                    break
-                if op == "*":
-                    start *= n
-                elif op == "+":
-                    start += n
-                elif op == "||":
-                    start = int(str(start) + str(n))
-                else:
-                    raise ValueError(op)
+            for point, direction in ((p2, 1), (p1, -1)):
+                if resonance:
+                    anti_nodes[point].append(frequency)
+                factor = 1
+                while True:
+                    node = (
+                        point[0] + direction * factor * dn,
+                        point[1] + direction * factor * dm,
+                    )
+                    if 0 <= node[0] < N and 0 <= node[1] < M:
+                        anti_nodes[node].append(frequency)
+                        factor += 1
+                        if not resonance:
+                            break
+                    else:
+                        break
 
-            if start == test:
-                sum_ += test
-                break
-    return sum_
+    return len(anti_nodes.items())
 
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
-    test_values, numbers = parse(lines)
-    print(f"Part 1: {run(test_values, numbers, operators=['+', '*'])}")
-    print(f"Part 2: {run(test_values, numbers, operators=['+', '*', '||'])}")
+    print(f"Part 1: {run(copy.deepcopy(lines), resonance=False)}")
+    print(f"Part 2: {run(copy.deepcopy(lines), resonance=True)}")
