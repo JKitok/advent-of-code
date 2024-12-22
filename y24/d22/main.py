261beb3381be546773355d61f5fd29de75be86f3
Jan-Erik Kitok <janerikkitok@gmail.com>
2024-12-22 09:38:06 +0100

    Add solution day 22 part 1

--- y24/d22/main.py
+++ y24/d22/main.py
@@ -1,11 +1,38 @@
 import os
 import copy
-from dataclasses import dataclass
-import numpy as np
+from tqdm import tqdm
+
+
+def mix(number, secret):
+    return number ^ secret
+
+
+def prune(number):
+    return number % 16777216
+
+
+def secret(number):
+    v = 64 * number
+    number = mix(v, number)
+    number = prune(number)
+
+    v = number // 32
+    number = mix(v, number)
+    number = prune(number)
+
+    v = number * 2048
+    number = mix(v, number)
+    return prune(number)
 
 
 def part1(lines):
-    pass
+    total = 0
+    for line in tqdm(lines):
+        num = int(line)
+        for i in range(2000):
+            num = secret(num)
+        total += num
+    return total
 
 
 def part2(lines):
@@ -13,7 +40,7 @@ def part2(lines):
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
     print(f"Part 1: {part1(copy.deepcopy(lines))}")
