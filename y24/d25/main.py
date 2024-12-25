1829bb079d15abefe0f53e086a9be39da62d0cb3
Jan-Erik Kitok <janerikkitok@gmail.com>
2024-12-25 09:56:27 +0100

    Add solution for day 25

--- y24/d25/main.py
+++ y24/d25/main.py
@@ -1,20 +1,33 @@
 import os
-import copy
-from dataclasses import dataclass
+from itertools import groupby
 import numpy as np
 
 
-def part1(lines):
-    pass
+def part1(objects):
+    locks = []
+    keys = []
+    for obj in objects:
+        obj = [list(v) for v in obj]
+        arr = np.array(obj, dtype=str)
+        as_int = np.zeros_like(arr, dtype=int)
+        as_int[arr == "#"] = 1
+        if as_int[0, 0] == 1:
+            locks.append(as_int)
+        else:
+            keys.append(as_int)
 
-
-def part2(lines):
-    pass
+    N = 0
+    for lock in locks:
+        for key in keys:
+            if not np.any((lock + key) > 1):
+                N += 1
+    return N
 
 
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
         lines = fp.readlines()
     lines = [v.rstrip("\n") for v in lines]
-    print(f"Part 1: {part1(copy.deepcopy(lines))}")
-    print(f"Part 2: {part2(copy.deepcopy(lines))}")
+    objects = [list(sub) for ele, sub in groupby(lines, key = bool) if ele]
+
+    print(f"Part 1: {part1(objects)}")
