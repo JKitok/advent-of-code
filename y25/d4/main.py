3e3ded5777f29eb565000e13829cf85453a11e33
Jan-Erik Kitok <janerikkitok@gmail.com>
2025-12-03 19:58:47 +0100

    Add empty files for 2025

---
+++ y25/d4/main.py
@@ -0,0 +1,18 @@
+import os
+import copy
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+if __name__ == "__main__":
+    with open(os.path.join(os.path.dirname(__file__), "example.txt")) as fp:
+        lines = fp.readlines()
+    lines = [v.rstrip("\n") for v in lines]
+    print(f"Part 1: {part1(copy.deepcopy(lines))}")
+    print(f"Part 2: {part2(copy.deepcopy(lines))}")
